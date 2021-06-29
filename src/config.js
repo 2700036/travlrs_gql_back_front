6b391188fcf46df4c7071a3a70a09f7e43340830
Sergey Biryukov <2700036@mail.ru>
2021-06-29 16:37:19 +0300

    upd

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ export const config = {
     url: process.env.MONGO_DB_URI || 'mongodb://localhost:27017/TrvlrsGQL_DB'
   },
   corsOptions: {
-    origin: `http://localhost:${process.env.PORT || 3000}`,
+    origin: `http://localhost:${process.env.PORT || 4000}`,
     credentials: true,
   },
   sessionOptions: {
@@ -18,7 +18,7 @@ export const config = {
     rolling: true,
     saveUninitialized: false,
     cookie: {
-      httpOnly: true,
+      
       maxAge: 15 * 60 *1000,
       domain: process.env.DOMAIN || 'http://localhost:3000'
     },
