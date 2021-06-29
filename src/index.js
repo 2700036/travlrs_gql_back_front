ce8f6fec3f49c587307918b18fb4a3e7bb16c5d1
Sergey Biryukov <2700036@mail.ru>
2021-06-29 17:32:39 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -14,7 +14,15 @@ dotenv.config()
 const app = express();
 
 app.use(session(config.sessionOptions));
-app.use(cors(config.corsOptions));
+// app.use(cors(config.corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+app.use(cors());
 app.use(readToken);
 const httpServer = http.createServer(app);
 
